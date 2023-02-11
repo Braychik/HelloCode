aaf5680d871bc99e369fcdb17a73898cbe4b8c95
Николай <ibraevnikolay@mail.ru>
2023-02-11 14:38:14 +0500

    add e[ample003_sum random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int namber1 = 6;
-int number2 = 14;
+﻿int namber1 = new Random().Next(1, 10);
+Console.WriteLine(namber1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
 int result = namber1 + number2;
 Console.Write(result);
