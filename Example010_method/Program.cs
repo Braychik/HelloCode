047cb39bdcc2ed0903883beec6abe9a666ae6d0c
Николай <ibraevnikolay@mail.ru>
2023-02-12 15:28:15 +0500

    добавили 10 папку также работа с массивом

---
+++ Example010_method/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11, 25, 13, 44, 35, 13, 71, 28};
+int n = array.Length;
+int find = 13;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1
+    index++;
+};
